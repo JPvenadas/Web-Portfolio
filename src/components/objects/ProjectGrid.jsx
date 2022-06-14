3b01446f244de8615a6ef1b2d74bfb4f851e186a
Jp Venadas <jaypheevenadas18@gmail.com>
2022-06-14 20:08:41 +0800

    plot the contents to each project

--- src/components/objects/ProjectGrid.jsx
+++ src/components/objects/ProjectGrid.jsx
@@ -8,37 +8,40 @@ import download from './../../Images/Download.png'
 
 //project images
 import portfolio from './../../Images/Project1.png'
+import slides from './../../Images/Project2.png'
+import easybank from './../../Images/Project3.png'
+import mynotepad from './../../Images/Project4.png'
 
 const ProjectGrid = () => {
   return (
     <Grid>
         <ProjectItem
          bg={portfolio}
-         title="Web Portfolioo"
+         title="Web Portfolio"
          description="A simple and minimalist Web Portfolio to showcase my projects and my skills as a developer and a designer. I Created it with Figma, React and fancy Greensock Animations"
          buttons={[{img: github},{img: web}]}
          Stacks={["React", "Figma", "Styled-components", "GSAP"]}
         />
         <ProjectItem
-         bg={portfolio}
-         title="Web Portfolioo"
-         description="A simple and minimalist Web Portfolio to showcase my projects and my skills as a developer and a designer. I Created it with Figma, React and fancy Greensock Animations"
+         bg={slides}
+         title="Slides"
+         description="A web game that I made for fun. As I love solving puzzles and playing Wild Rift. I created a classic sliding puzzle game with League of Legends theme. It has three level of difficulty and 50 different images"
          buttons={[{img: github},{img: web}]}
-         Stacks={["React", "Figma", "Styled-components", "GSAP"]}
+         Stacks={["HTML", "CSS", "Vanilla Javascript"]}
         />
          <ProjectItem
-         bg={portfolio}
-         title="Web Portfolioo"
-         description="A simple and minimalist Web Portfolio to showcase my projects and my skills as a developer and a designer. I Created it with Figma, React and fancy Greensock Animations"
+         bg={[easybank]}
+         title="EasyBank"
+         description="A simple landing page interface. An intermidiate challenge from Frontend Mentor that I take to test wether I could develop/convert a design(Picture) to a website Interface."
          buttons={[{img: github},{img: web}]}
-         Stacks={["React", "Figma", "Styled-components", "GSAP"]}
+         Stacks={["React", "Styled-components"]}
         />
          <ProjectItem
-         bg={portfolio}
-         title="Web Portfolioo"
-         description="A simple and minimalist Web Portfolio to showcase my projects and my skills as a developer and a designer. I Created it with Figma, React and fancy Greensock Animations"
-         buttons={[{img: github},{img: web}]}
-         Stacks={["React", "Figma", "Styled-components", "GSAP"]}
+         bg={mynotepad}
+         title="My Notepad"
+         description="Have an app that can be your notepad everytime. this is an android CRUD application that I made with React native. It is made to keep and manage your daily notes."
+         buttons={[{img: github},{img: download}]}
+         Stacks={["React Native", "Async Storage", "Formik"]}
         />
     </Grid>
   )
