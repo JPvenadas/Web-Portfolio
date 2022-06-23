67089161c6733ab2fc7951373bdabfb30c8f39ec
Jp Venadas <jaypheevenadas18@gmail.com>
2022-06-23 13:36:16 +0800

    make the nav logo clickable

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     <MainContainer className="navbar">
       <Container>
             <LogoContainer>
-              <Logo className="logo">Jp Venadas.</Logo>
+              <a href="#home"><Logo className="logo">Jp Venadas.</Logo></a>
             </LogoContainer>
             <NavLinksContainer background={navopen? "#97AA90": "#EEF0EB"} right={navopen? "0": "-400px" }>
               <Navlinks right={navopen? "0": "-200px" }>
